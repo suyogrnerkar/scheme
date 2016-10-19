5e58f18a87da09180d1ec2b25de6b173232a6926
Suyog Nerkar <suyogrnerkar@gmail.com>
2016-10-18 20:17:08 -0400

    Q1 Parser Completed.

--- files/ugly-regexp/parser.rkt
+++ files/ugly-regexp/parser.rkt
@@ -22,41 +22,88 @@
 ;;If error, catch syntax-error exception, print message and recover.
 (define (parse)
   (unless (equal? '<EOF> (token-kind *lookahead*))
-	  (with-handlers
-	    ([exn:fail:user?
-	      (lambda (ex)
-		(do-error (exn-message ex))
-		(recover-from-error)
-		(advance))])
-	    (let ([out (ugly-regexp)])
-	      (when (check? '<NL>) (displayln out))
-	      (match '<NL>)))
-	  (parse)))
+    (with-handlers
+        ([exn:fail:user?
+          (lambda (ex)
+            (do-error (exn-message ex))
+            (recover-from-error)
+            (advance))])
+      (let ([out (ugly-regexp)])
+        (when (check? '<NL>) (displayln out))
+        (match '<NL>)))
+    (parse)))
 
+
+;; Function for UGLY-REGEXP
 (define (ugly-regexp)
   (let ([t (term)])
     (regexp-rest t)))
 
+
+;; Function for REGEXP-REST
 (define (regexp-rest t)
   (if (check? 'CHAR ".")
       (begin
-	(match 'CHAR ".")
-	(let ([t1 (term)])
-	  (regexp-rest (string-append "(" t t1 ")"))))
+        (match 'CHAR ".")
+        (let ([t1 (term)])
+          (regexp-rest (string-append "(" t t1 ")"))))
       t))
 
-;;Flesh out skeleton functions below.
+
+;; Function for TERM
 (define (term)
-  '())
+  (let ([f (factor)])
+    (term-rest f)))
 
+
+;; Function for TERM REST
 (define (term-rest f)
-  '())
+  (if (check? 'CHAR "+")
+      (begin
+        (match 'CHAR "+")
+        (let ([f1 (factor)])
+          (term-rest (string-append "(" f "|" f1 ")"))
+          ))
+      f))
 
+
+;; Function for FACTOR
 (define (factor)
-  '())
+  (cond 
+    [(check? 'CHAR "(")
+      (begin
+        (match 'CHAR "(")
+        (let ([e (ugly-regexp)])
+        (match 'CHAR ")")
+        (string-append "(" e ")")))
+    ]         
+    [(check? 'CHAR "*")
+      (begin
+        (match 'CHAR "*")
+        (let ([ f (factor) ])
+        (string-append f "*")))
+    ]
+    [else
+      (match 'CHARS)
+      (match 'CHAR "(")
+      (let ([ s (token-lexeme *lookahead*) ])
+      (match 'CHAR)
+      (let ([chars (chars (quote-char s))])
+      (match 'CHAR ")")
+      (string-append "[" chars "]")))
 
+    ]))
+
+
+;; Function for CHARS
 (define (chars s)
-  '())
+  (if (check? 'CHAR ",")
+      (begin
+        (match 'CHAR ",")
+        (let ([s1 (token-lexeme *lookahead*)]) 
+          (match 'CHAR)
+          (chars (string-append s (quote-char s1)))))
+      s))
 
 ;;;********************** Parser Utility Functions *********************
 
@@ -74,7 +121,7 @@
 (define (check? kind . rest)
   (and (equal? kind (token-kind *lookahead*))
        (or (equal? (length rest) 0)
-	   (equal? (car rest) (token-lexeme *lookahead*)))))
+           (equal? (car rest) (token-lexeme *lookahead*)))))
 
 ;;quote first char of string if not word-char or digit.
 (define (quote-char str)
@@ -90,16 +137,16 @@
 ;;which is either (KIND) or (KIND LEXEME)
 (define (make-error-message args)
   (let* ([kind (car args)]
-	 [expected (if (equal? (length args) 2) (cadr args) kind)])
+         [expected (if (equal? (length args) 2) (cadr args) kind)])
     (format "~a: syntax error at '~a', expecting '~s'"
-	    (position->string (token-position *lookahead*))
-	    (token-lexeme *lookahead*)
-	    expected)))
+            (position->string (token-position *lookahead*))
+            (token-lexeme *lookahead*)
+            expected)))
 
 (define (recover-from-error)
   (unless (or (check? '<NL>) (check? '<EOF>))
-	  (advance)
-	  (recover-from-error)))
+    (advance)
+    (recover-from-error)))
 
 ;;;*************************** Main Program ****************************
 
@@ -107,11 +154,11 @@
   (if (not (equal? (vector-length argv) 1))
       (usage)
       (begin
-	(set! *next-token* (scanner (vector-ref argv 0)))
-	(advance)    ;prime lookahead
-	(parse))))
-       
+        (set! *next-token* (scanner (vector-ref argv 0)))
+        (advance)    ;prime lookahead
+        (parse))))
+
 (run-parse (current-command-line-arguments))
-				
-				
+
+
 
